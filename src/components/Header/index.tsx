c64638854e2d0c2101fd054ad72a319366fb6554
JrzenonDev <jrobertoonb@gmail.com>
2023-01-24 14:41:45 -0300

    feat: use component ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import logo from '../../../public/images/logo.svg';
+import { ActiveLink } from '../ActiveLink';
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -9,19 +9,27 @@ export function Header() {
       <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
 
-          <a>
-            <Image
-              src={logo}
-              alt='Logo blog'
-              width={248}
-              height={72}
-            />
-          </a>
+          <ActiveLink href='/' activeClassName={styles.active}>
+            <a>
+              <Image
+                src={logo}
+                alt='Logo blog'
+                width={248}
+                height={72}
+              />
+            </a>
+          </ActiveLink>
 
           <nav>
-            <Link href='/'>Home</Link>
-            <Link href='/posts'>Conteúdo</Link>
-            <Link href='/sobre'>Quem Somos</Link>
+            <ActiveLink href='/' activeClassName={styles.active}>
+              <a>Home</a>
+            </ActiveLink>
+            <ActiveLink href='/posts' activeClassName={styles.active}>
+              <a>Conteúdo</a>
+            </ActiveLink>
+            <ActiveLink href='/sobre' activeClassName={styles.active}>
+              <a>Quem Somos</a>
+            </ActiveLink>
           </nav>
 
           <a className={styles.readyButton} href='https://' type='button'>Começar</a>
