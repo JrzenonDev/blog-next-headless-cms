55c5f08def95c692df5f54921037d24828d3fc05
JrzenonDev <jrobertoonb@gmail.com>
2023-01-26 00:56:01 -0300

    feat: init page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Head>
+        <title>Apaixonado porJrZenonDev</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando você ao próximo nível</h1>
+            <span>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error officiis harum eaque quod omnis minus, eum odio temporibus quisquam quis cum nihil ducimus hic labore fuga incidunt et cupiditate quo.
+            </span>
+            <a>
+              <button>
+                COMEÇAR AGORA!
+              </button>
+            </a>
+          </section>
+          <img src='/images/banner-conteudos.png' alt='Conteúdos de tecnologia' />
+        </div>
+      </main>
+    </>
   )
 }
